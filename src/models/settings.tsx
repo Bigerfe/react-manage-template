cd5757d9e5ba51b19f9f71ae62b11653cb23fd60
zhangjiapeng04 <zhangjiapeng04@meituan.com>
2023-07-27 15:03:37 +0800

    初始化react管理端模板

---
+++ src/models/settings.tsx
@@ -0,0 +1,15 @@
+import type { ProSettings } from '@ant-design/pro-components';
+
+const themeSettins: ProSettings = {
+  fixSiderbar: true,
+  layout: "mix",
+  splitMenus: false,
+  navTheme: "light",
+  contentWidth: "Fluid",
+  colorPrimary: "#1677FF",
+  fixedHeader: false,
+  siderMenuType: "sub",
+  footerRender: false
+}
+
+export default themeSettins;
